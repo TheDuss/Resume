57d9bb4b133c40b898aa109dd396ad6ae2d4fce6
TheDuss <murtazaal92@gmail.com>
2024-08-20 09:26:42 +0200

    replacement  made in the lists of work,voluntarywork and education

--- Resume-project/src/app/about/about.component.ts
+++ Resume-project/src/app/about/about.component.ts
@@ -3,12 +3,11 @@ import { Component } from '@angular/core';
 import { WorkComponent } from '../work/work.component';
 import { VoluntaryworkComponent } from '../voluntarywork/voluntarywork.component';
 import { EducationComponent } from '../education/education.component';
-import { TestComponent } from '../test/test.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [WorkComponent,VoluntaryworkComponent,EducationComponent,TestComponent],
+  imports: [WorkComponent,VoluntaryworkComponent,EducationComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
