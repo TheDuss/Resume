e907bc97afd7c411b5886fada002ef951f9b5b27
TheDuss <murtazaal92@gmail.com>
2024-08-20 20:20:32 +0200

    changede the box size and moved profile description to about, and added download CV btn

--- Resume-project/src/app/about/about.component.ts
+++ Resume-project/src/app/about/about.component.ts
@@ -3,14 +3,27 @@ import { Component } from '@angular/core';
 import { WorkComponent } from '../work/work.component';
 import { VoluntaryworkComponent } from '../voluntarywork/voluntarywork.component';
 import { EducationComponent } from '../education/education.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [WorkComponent,VoluntaryworkComponent,EducationComponent],
+  imports: [WorkComponent,VoluntaryworkComponent,EducationComponent,HttpClientModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
 export class AboutComponent {
-
+  constructor(private http: HttpClient) {}
+  downloadFile() {
+    const fileUrl = '/assets/CV.pdf';
+  
+    this.http.get(fileUrl, { responseType: 'blob' }).subscribe((blob) => {
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'CV.pdf';
+      a.click();
+      window.URL.revokeObjectURL(url);
+    });
+  }
 }
